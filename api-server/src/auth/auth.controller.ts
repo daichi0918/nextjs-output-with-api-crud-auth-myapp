e69afc88df499213455a6562477e41911d9b1f14
yuki <yuki.onishi1129@gmail.com>
2022-10-26 23:08:35 +0900

    add: sign in api

--- api-server/src/auth/auth.controller.ts
+++ api-server/src/auth/auth.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  HttpCode,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInUserDto } from './dto/sign-in-user.dto';
+import { LocalAuthGuard } from '../lib/guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('sign_in')
+  @HttpCode(200)
+  async signIn(@Body(ValidationPipe) signInUserDto: SignInUserDto) {
+    return await this.authService.signIn(signInUserDto);
+  }
 }
--- api-server/src/auth/auth.service.ts
+++ api-server/src/auth/auth.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { SignInUserDto } from './dto/sign-in-user.dto';
+import { PrismaService } from '../prisma.service';
+import { JwtPayload } from '../lib/jwt/interfaces/jwt-payload.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private readonly jwtSecret: JwtService,
+  ) {}
+
+  async signIn(signInUserDto: SignInUserDto) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email: signInUserDto.email,
+      },
+    });
+    // パスワード照合
+    if (
+      !user ||
+      !(await bcrypt.compare(signInUserDto.password, user.password))
+    ) {
+      throw new UnauthorizedException(
+        'メールアドレスまたはパスワードが違います',
+      );
+    }
+
+    const payload: JwtPayload = {
+      userId: user.id,
+      email: user.email,
+    };
+
+    // jwtアクセストークンを作成し返却
+    return {
+      accessToken: this.jwtSecret.sign(payload),
+    };
+  }
+}
