d6cd2d38b15f94f853485adebcd6dd7f914591a0
yuki <yuki.onishi1129@gmail.com>
2022-10-27 14:57:19 +0900

    add: sign up api

--- api-server/src/auth/auth.controller.ts
+++ api-server/src/auth/auth.controller.ts
@@ -4,20 +4,25 @@ import {
   Body,
   ValidationPipe,
   HttpCode,
-  UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInUserDto } from './dto/sign-in-user.dto';
-import { LocalAuthGuard } from '../lib/guards/local-auth.guard';
+import { SignUpUserDto } from './dto/sign-up-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
   @Post('sign_in')
   @HttpCode(200)
   async signIn(@Body(ValidationPipe) signInUserDto: SignInUserDto) {
     return await this.authService.signIn(signInUserDto);
+  }
+
+  @Post('sign_up')
+  @HttpCode(200)
+  async signUp(@Body(ValidationPipe) signUpUserDto: SignUpUserDto) {
+    console.log('aaaaaa');
+    return await this.authService.signUp(signUpUserDto);
   }
 }
--- api-server/src/auth/auth.service.ts
+++ api-server/src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { SignInUserDto } from './dto/sign-in-user.dto';
+import { SignUpUserDto } from './dto/sign-up-user.dto';
 import { PrismaService } from '../prisma.service';
 import { JwtPayload } from '../lib/jwt/interfaces/jwt-payload.interface';
 
@@ -31,6 +32,38 @@ export class AuthService {
     const payload: JwtPayload = {
       userId: user.id,
       email: user.email,
+    };
+
+    // jwtアクセストークンを作成し返却
+    return {
+      accessToken: this.jwtSecret.sign(payload),
+    };
+  }
+
+  async signUp(signUpUserDto: SignUpUserDto) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email: signUpUserDto.email,
+      },
+    });
+    // メールアドレス確認
+    if (!!user)
+      throw new UnauthorizedException(
+        `${signUpUserDto.email} は別のアカウントで使用されています。`,
+      );
+
+    const hashPassword = await bcrypt.hash(signUpUserDto.password, 10);
+    const createdUser = await this.prisma.user.create({
+      data: {
+        name: signUpUserDto.name,
+        email: signUpUserDto.email,
+        password: hashPassword,
+      },
+    });
+
+    const payload: JwtPayload = {
+      userId: createdUser.id,
+      email: createdUser.email,
     };
 
     // jwtアクセストークンを作成し返却
