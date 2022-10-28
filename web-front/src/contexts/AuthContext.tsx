3b5adbfd819ea8b125ae4aaf6149523517fac370
yuki <yuki.onishi1129@gmail.com>
2022-10-28 15:47:05 +0900

    add: useAuth

--- web-front/src/contexts/AuthContext.tsx
+++ web-front/src/contexts/AuthContext.tsx
@@ -3,11 +3,9 @@
  *
  * @package contexts
  */
-import { FC, ReactNode, useState, useCallback, useContext, createContext, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { authenticationApi } from '@/apis/authApi';
+import { FC, ReactNode, useContext, createContext } from 'react';
 import { UserType } from '@/interfaces/User';
-import { NAVIGATION_LIST, NAVIGATION_PATH } from '@/constants/navigation';
+import { useAuth } from '@/hooks/useAuth';
 
 type Props = {
   children: ReactNode;
@@ -28,48 +26,7 @@ const AuthContext = createContext({} as ContextInterface);
  * @returns
  */
 export const AuthProvider: FC<Props> = ({ children }) => {
-  const router = useRouter();
-  const [user, setUser] = useState<UserType | undefined>(undefined);
-  const [isAuth, setIsAuth] = useState<boolean>(false);
-
-  const singIn = useCallback(async (user: UserType) => {
-    setUser(user);
-    setIsAuth(true);
-  }, []);
-
-  const signOut = useCallback(async () => {
-    setUser(undefined);
-    setIsAuth(false);
-  }, []);
-
-  /**
-   * 未ログインページにいるか判定処理
-   */
-  const isExitBeforeAuthPage = useCallback(() => {
-    return router.pathname === NAVIGATION_PATH.SIGNIN || router.pathname === NAVIGATION_PATH.SIGNUP;
-  }, [router.pathname]);
-
-  /**
-   * 認証ルーティング
-   */
-  const authRouting = useCallback(async () => {
-    let auth = false;
-    const res = await authenticationApi();
-    if (res?.data?.user) {
-      setUser(res?.data?.user);
-      setIsAuth(true);
-      auth = true;
-    }
-
-    // 未ログインでログイン後のページにいる場合、ログイン画面にリダイレクト
-    if (!auth && !isExitBeforeAuthPage()) router.push(NAVIGATION_LIST.SIGNIN);
-    // ログイン済で未ログインのページにいる場合、Todo一覧ページにリダイレクト
-    if (auth && isExitBeforeAuthPage()) router.push(NAVIGATION_LIST.TOP);
-  }, [isExitBeforeAuthPage, router]);
-
-  useEffect(() => {
-    authRouting();
-  }, [authRouting]);
+  const { user, isAuth, singIn, signOut } = useAuth();
 
   return (
     <AuthContext.Provider
