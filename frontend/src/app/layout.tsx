2a715a810ea582a0d574e3bf2e98d452079aaf2e
daichi0918 <kayasugadaichi@icloud.com>
2024-12-02 10:34:50 +0900

    todoContext作成完了

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import { AuthProvider } from '@/contexts/AuthContext';
+import { TodoProvider } from '@/contexts/TodoContext';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -28,7 +29,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <AuthProvider>
-          <div className="container">{children}</div>
+          <TodoProvider>
+            <div className="container">{children}</div>
+          </TodoProvider>
         </AuthProvider>
       </body>
     </html>
